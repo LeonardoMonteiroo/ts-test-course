27d0c76afccbf38f9f5424891d6280942b2287e2
Leonardo Monteiro <leonardo.silva@clinicorp.com>
2026-01-13 21:07:24 -0300

    Update Utils and test cases

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,4 +1,4 @@
-import { toUpperCase } from "../app/Utils";
+import { getStringInfo, toUpperCase } from "../app/Utils";
 
 describe('Utils test suite', () => {
   it('should return uppercase of valid string', () => {
@@ -8,5 +8,27 @@ describe('Utils test suite', () => {
     const actual = sut('hello');
 
     expect(actual).toBe(expected);
+  })
+
+  it('should return string info if valid string is provided', () => {
+    const sut = getStringInfo;
+
+    const actual = sut('My-String');
+
+    expect(actual.lowerCase).toBe('my-string');
+    expect(actual.upperCase).toBe('MY-STRING');
+    expect(actual.extraInfo).toEqual({});
+
+    expect(actual.characters.length).toBe(9);
+    expect(actual.characters).toHaveLength(9);
+
+    expect(actual.characters).toEqual(['M', 'y', '-', 'S', 't', 'r', 'i', 'n', 'g']);
+    expect(actual.characters).toContain<string>('S');
+    expect(actual.characters).toEqual(expect.arrayContaining(['S', 't', 'r', 'i', 'n', 'g', 'M', 'y', '-']));
+
+    expect(actual.extraInfo).not.toBe(undefined);
+    expect(actual.extraInfo).not.toBeUndefined();
+    expect(actual.extraInfo).toBeDefined();
+    expect(actual.extraInfo).toBeTruthy();
   })
 });
